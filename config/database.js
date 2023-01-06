6244c8f58b81fd0dfe18839845c4f950b7ec1eb2
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-05 23:17:04 -0800

    created database config

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('dbTutorialPt', 'root', process.env.LOCALHOST_PASSWORD, {
+    host: "localhost",
+    dialect: "mysql",
+    logging: false,
+});
+
+const testDBConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+testDBConnection();
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -2,5 +2,13 @@ const express = require('express');
 const app = express(); //initialize express
 const cors = require("cors");
 require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+
+const sequelize = require("./config/database"); 
+
+const PORT = process.env.port || 3001;
+
+app.listen(PORT, () => console.log("LISTENING TO PORT", PORT))
+
