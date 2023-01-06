1aa999a04823517d7be69a5aeed81aa030295db9
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-06 00:49:02 -0800

    updated db config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('moby-local', 'root', process.env.LOCALHOST_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
+    host: process.env.DB_HOST, 
+    dialect: "mysql", 
+    logging: false, 
+    port: process.env.DB_PORT 
 });
+// const sequelize = new Sequelize('moby-local', 'root', process.env.LOCALHOST_PASSWORD, {
+//     host: "localhost",
+//     dialect: "mysql",
+//     logging: false,
+// });
 
 const testDBConnection = async () => {
     try {
